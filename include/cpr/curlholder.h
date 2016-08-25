6a294e2c041820fbb6590be63808f7217848bd6e
dlee <lbqq0000@gmail.com>
2016-08-25 18:34:24 +0800

    use functor instead of std::function

--- include/cpr/curlholder.h
+++ include/cpr/curlholder.h
@@ -7,16 +7,24 @@
 
 namespace cpr {
 
+    struct curl_slist_deleter {
+        void operator()(struct curl_slist* ptr) {
+            curl_slist_free_all(ptr);
+        }
+    };
+
+    struct curl_form_deleter {
+        void operator()(struct curl_httppost* ptr) {
+            curl_formfree(ptr);
+        }
+    };
+
 struct CurlHolder {
     CURL* handle;
-    std::unique_ptr<struct curl_slist, std::function<void(struct curl_slist*)>> chunk;
-    std::unique_ptr<struct curl_httppost, std::function<void(struct curl_httppost*)>> formdata;
+    std::unique_ptr<struct curl_slist, curl_slist_deleter> chunk;
+    std::unique_ptr<struct curl_httppost, curl_form_deleter> formdata;
     char error[CURL_ERROR_SIZE];
-    CurlHolder(): chunk(nullptr, [](struct curl_slist* ptr) {
-        curl_slist_free_all(ptr);
-    }), formdata(nullptr, [](struct curl_httppost* ptr) {
-        curl_formfree(ptr);
-    }) {
+    CurlHolder(): chunk(nullptr), formdata(nullptr) {
     }
 };
 
